920c2bafa8f2128333962bb5ec467eca1618f242
Uli Kusterer <ulivc@zathras.de>
2016-04-03 17:36:43 +0200

    Initial Commit

---
+++ CreepyCrawly/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CreepyCrawly
+//
+//  Created by Uli Kusterer on 03/04/16.
+//  Copyright © 2016 Uli Kusterer. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+    return 0;
+}
